62aec6a5ce39036098f87bac2a100107046fabf6
Yixuan-Wang <21151119+Yixuan-Wang@users.noreply.github.com>
2021-07-17 00:34:11 +0800

    feat: merge posts and talks on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,27 +29,18 @@ export const query = graphql`
         description
       }
     }
-    recentPosts: allMarkdownRemark(
-      limit: 3
-      filter: { fields: { quarter: { eq: "posts" } } }
+    recents: allMarkdownRemark(
+      limit: 10
+      filter: { fields: { quarter: { in: ["posts", "talks"] } } }
       sort: { fields: frontmatter___date, order: DESC }
     ) {
       nodes {
         ...Article
       }
     }
-    recentSheets: allMarkdownRemark(
-      limit: 1
+    sheets: allMarkdownRemark(
+      limit: 3
       filter: { fields: { quarter: { eq: "sheets" } } }
-      sort: { fields: frontmatter___date, order: DESC }
-    ) {
-      nodes {
-        ...Article
-      }
-    }
-    recentTalks: allMarkdownRemark(
-      limit: 3
-      filter: { fields: { quarter: { eq: "talks" } } }
       sort: { fields: frontmatter___date, order: DESC }
     ) {
       nodes {
@@ -70,30 +61,24 @@ const IndexPage = ({ data }) => {
       <h1 className="title">{siteMetadata.title}</h1>
       <p className="subtitle description">{siteMetadata.description}</p>
       <section className="index-block">
-        <h2 id="recent-talks">最近的言论</h2>
-        {data.recentTalks.nodes.map(article => (
+        <h2 id="recents">文章和言论</h2>
+        {data.recents.nodes.map(article => (
           <CardArticle key={article.id} article={article} />
         ))}
         <span className="index-link-more">
-          <Link to="/talks/">全部…</Link>
+          <Link to="/posts/">全部文章…</Link>
+        </span>
+        <span className="index-link-more">
+          <Link to="/talks/">全部言论…</Link>
         </span>
       </section>
       <section className="index-block">
-        <h2 id="recent-posts">最近的文章</h2>
-        {data.recentPosts.nodes.map(article => (
+        <h2 id="recent-sheets">清单</h2>
+        {data.sheets.nodes.map(article => (
           <CardArticle key={article.id} article={article} />
         ))}
         <span className="index-link-more">
-          <Link to="/posts/">全部…</Link>
-        </span>
-      </section>
-      <section className="index-block">
-        <h2 id="recent-sheets">最近的清单</h2>
-        {data.recentSheets.nodes.map(article => (
-          <CardArticle key={article.id} article={article} />
-        ))}
-        <span className="index-link-more">
-          <Link to="/sheets/">全部…</Link>
+          <Link to="/sheets/">全部清单…</Link>
         </span>
       </section>
       <section className="index-block">
@@ -104,7 +89,7 @@ const IndexPage = ({ data }) => {
           ))}
         </div>
         <span className="index-link-more">
-          <Link to="/friends">更多…</Link>
+          <Link to="/friends">详细…</Link>
         </span>
       </section>
     </>
