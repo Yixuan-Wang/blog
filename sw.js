585a57c5c485335fa45cb4acad6c92bf4699572e
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2021-07-18 08:07:18 +0000

    deploy: 6c18f6f04a8a8ae37c13da50cb90a9dd4915322c

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-177ae7c7a1fb300cb898.js"
+    "url": "webpack-runtime-cc49479222c3badea9bc.js"
   },
   {
     "url": "styles.ba6d06f4d1bda1c8e6fc.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bcfe445e82d89193e668dc57631363d3"
+    "revision": "d655e738ae70585255c0bad74eb8b657"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -58,7 +58,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "aacc411e8780757f4422d8fcea22bc27"
+    "revision": "9f65068d72b65b13b0010e82651ca8f5"
   },
   {
     "url": "polyfill-2cb897c2a056d28c9332.js"
