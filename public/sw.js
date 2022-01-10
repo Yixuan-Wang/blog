b877636f187af57f98a9a50634ac1eed6215cff4
Yixuan-Wang <21151119+Yixuan-Wang@users.noreply.github.com>
2022-01-10 22:28:24 +0800

    fix: attempt 2 to remove v1 pwa

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,14 @@
 // remove all service workers
-navigator.serviceWorker.getRegistrations().then(function(registrations) {
-  for(let registration of registrations) {
-    registration.unregister()
-}})
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    });
+});
