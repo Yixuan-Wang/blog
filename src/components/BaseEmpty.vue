4e2ada8b05c6ec934a732c7d60247a6cd7859cfe
Yixuan-Wang <21151119+Yixuan-Wang@users.noreply.github.com>
2022-01-11 10:07:33 +0800

    fix: BaseEmpty white-space

--- src/components/BaseEmpty.vue
+++ src/components/BaseEmpty.vue
@@ -3,9 +3,13 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <div>
-    <mdi-broom class="text-4xl" /><br />
-    <span class="text-lg">{{ t('empty') }}</span><br />
-    <span class="text-dim whitespace-pre">{{ t('empty-aphorism') }}</span>
+  <div class="flex flex-col gap-1">
+    <mdi-broom class="text-4xl" />
+    <h2 class="text-lg">
+      {{ t('empty') }}
+    </h2>
+    <p class="text-dim whitespace-pre-wrap">
+      {{ t('empty-aphorism') }}
+    </p>
   </div>
 </template>
