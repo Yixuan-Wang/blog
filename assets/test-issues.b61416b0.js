d374721752cb560de21d700e1b39085051c097c8
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-02-17 12:00:21 +0000

    deploy: 01ae18d6ff31c44e0dbc84ff6368befac20ae458

---
+++ assets/test-issues.b61416b0.js
@@ -0,0 +1,11 @@
+import{_ as n}from"./TheArticle.692fa083.js";import{o,e as r}from"./vendor.c722daca.js";import"./app.3b59a81a.js";const h="Issues \u6D4B\u8BD5",g="2021-02-19",f="misc",u=["test"],_={setup(a,{expose:e}){const t={title:"Issues \u6D4B\u8BD5",date:"2021-02-19",category:"misc",tags:["test"]};return e({frontmatter:t}),(c,m)=>{const s=n;return o(),r(s,{frontmatter:t,inner:`<div><p>Issues API \u7684\u6D4B\u8BD5\u3002</p>
+<!-- more -->
+<h2 id="\u6D4B\u8BD5" tabindex="-1">\u6D4B\u8BD5</h2>
+<p><img src="https://user-images.githubusercontent.com/21151119/108468630-ffc09d80-72c1-11eb-8ddb-58154e85e17c.png" alt="\u8EAB\u4EFD"></p>
+<h2 id="comment-test" tabindex="-1">Comment Test</h2>
+<p>This is a <strong>test</strong> comment.</p>
+<hr class="talk-separator" />
+<h2 id="another-comment-test" tabindex="-1">Another Comment Test</h2>
+<p>This is <em>another</em> <strong>test</strong> comment.<br>
+And it is used for CI tests.</p>
+</div>`})}}};export{f as category,g as date,_ as default,u as tags,h as title};
