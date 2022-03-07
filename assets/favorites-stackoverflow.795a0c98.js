5bddc1fa41e8916f94667fd008cf4c3ce38d86e2
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-03-07 03:42:03 +0000

    deploy: ab8122ebea6296e43f8db88817bc0a7ed805134e

---
+++ assets/favorites-stackoverflow.795a0c98.js
@@ -0,0 +1,28 @@
+import{_ as a}from"./TheArticle.10df555b.js";import{o as n,e as r}from"./vendor.68320762.js";import"./app.124b5007.js";const p="StackOverflow \u95EE\u9898\u6536\u85CF\u5939",g="2021-10-01T00:00:00.000Z",m="comp",u=["utils","pl","tex"],k={setup(i,{expose:t}){const e={title:"StackOverflow \u95EE\u9898\u6536\u85CF\u5939",date:"2021-10-01T00:00:00.000Z",category:"comp",tags:["utils","pl","tex"]};return t({frontmatter:e}),(s,l)=>{const o=a;return n(),r(o,{frontmatter:e,inner:`<div><p>\u8BA9\u7A0B\u5E8F\u5458\u5F00\u5FC3\u7684 StackOverflow \u95EE\u9898\uFF0C\u6284\u4E86\u53C8\u6284\u3002</p>
+<!-- more -->
+<h2 id="memes" tabindex="-1">Memes</h2>
+<p><a href="https://stackoverflow.com/questions/11828270/how-do-i-exit-the-vim-editor" target="_blank" rel="noopener">How do I exit the Vim editor?</a></p>
+<hr class="talk-separator" />
+<h2 id="latex" tabindex="-1">LaTeX</h2>
+<ul>
+<li>\u4F2A\u4EE3\u7801\u5B8F\u5305\u4ECB\u7ECD - <a href="https://stackoverflow.com/questions/11828270/how-do-i-exit-the-vim-editor" target="_blank" rel="noopener">algorithm, algorithmic, algorithmicx, algorithm2e, algpseudocode = confused</a></li>
+<li>\u4F2A\u4EE3\u7801\u6D6E\u52A8\u4F53\u5206\u9875 - <a href="https://tex.stackexchange.com/questions/33866/algorithm-tag-and-page-break" target="_blank" rel="noopener">Algorithm tag and page break</a></li>
+<li>TikZ \u7ED8\u5236\u5E26\u6743\u56FE - <a href="https://tex.stackexchange.com/questions/166083/showing-weights-on-tikz-graph-using-draw-edge-commands" target="_blank" rel="noopener">Showing weights on Tikz graph using \u201Cdraw edge\u201D commands</a></li>
+</ul>
+<hr class="talk-separator" />
+<h2 id="linux-unix" tabindex="-1">Linux &amp; Unix</h2>
+<ul>
+<li>zsh \u7684\u914D\u7F6E\u6587\u4EF6 - <a href="https://unix.stackexchange.com/questions/71253/what-should-shouldnt-go-in-zshenv-zshrc-zlogin-zprofile-zlogout" target="_blank" rel="noopener">What should/shouldn\u2019t go in .zshenv, .zshrc, .zlogin, .zprofile, .zlogout?</a></li>
+</ul>
+<hr class="talk-separator" />
+<h2 id="python" tabindex="-1">Python</h2>
+<h3 id="\u79D1\u5B66\u8BA1\u7B97" tabindex="-1">\u79D1\u5B66\u8BA1\u7B97</h3>
+<ul>
+<li>\u9010\u884C\u904D\u5386 <code>pandas.DataFrame</code> - <a href="https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas" target="_blank" rel="noopener">How to iterate over rows in a DataFrame in Pandas</a></li>
+</ul>
+<hr class="talk-separator" />
+<h2 id="rust" tabindex="-1">Rust</h2>
+<ul>
+<li>\u5B57\u7B26\u4E32\u76F8\u5173\u7C7B\u578B\u7684\u8F6C\u6362 - <a href="https://stackoverflow.com/questions/41034635/how-do-i-convert-between-string-str-vecu8-and-u8" target="_blank" rel="noopener">How do I convert between <code>String</code>, <code>&amp;str</code>, <code>Vec&lt;u8&gt;</code> and <code>&amp;[u8]</code>?</a></li>
+</ul>
+</div>`})}}};export{m as category,g as date,k as default,u as tags,p as title};
---
+++ assets/jpn-lyrics-gloss-a.60f47b6f.js
@@ -0,0 +1,89 @@
+import{_ as u}from"./TheArticle.10df555b.js";import{o as a,e}from"./vendor.68320762.js";import"./app.124b5007.js";const l="\u65E5\u8BED\u6B4C\u8BCD\u6807\u6CE8\u4E4B\u7532",g="2022-03-05T00:00:00.000Z",d="misc",m=["lyrics"],c=["lang:jpn","folk"],k={setup(b,{expose:n}){const r={title:"\u65E5\u8BED\u6B4C\u8BCD\u6807\u6CE8\u4E4B\u7532",date:"2022-03-05T00:00:00.000Z",category:"misc",tags:["lyrics"],keywords:["lang:jpn","folk"]};return n({frontmatter:r}),(y,i)=>{const t=u;return a(),e(t,{frontmatter:r,inner:`<div><p>\u504F\u5411\u6C11\u8C23\u6027\u8D28\u7684\u65E5\u8BED\u6B4C\u6B4C\u8BCD\u6807\u6CE8\u3002</p>
+<!-- more -->
+<h2 id="\u7D3A\u8272\u306E\u3046\u306D\u308A\u304C" tabindex="-1">\u7D3A\u8272\u306E\u3046\u306D\u308A\u304C</h2>
+<p>\u539F\u4F5C\u3000\u5BAE\u6CA2\u8CE2\u6CBB\u300C\u751F\u5F92\u8AF8\u541B\u306B\u5BC4\u305B\u308B\u300D<br>
+\u3000\u8BCD\u3000\u5BAE\u5D0E\u99FF\u30FB\u5BAE\u5D0E\u543E\u6717<br>
+\u3000\u66F2\u3000\u8C37\u5C71\u6D69\u5B50<br>
+\u7F16\u66F2\u3000\u6B66\u90E8\u8061\u5FD7<br>
+\u3000\u6B4C\u3000\u624B\u5D8C\u8475</p>
+<details><summary>\u6B4C\u8BCD</summary>
+<div style="line-height:2.25em;padding:1em 0em" class="%">
+<p><ruby>\u7D3A\u8272<rt>\u3053\u3093\u3044\u308D</rt></ruby>\u306E\u3046\u306D\u308A\u304C<br>
+\u306E\u307F\u3064\u304F\u3059<ruby>\u65E5<rt>\u3072</rt></ruby>\u304C<ruby>\u6765<rt>\u304D</rt></ruby>\u3066\u3082<br>
+<ruby>\u6C34\u5E73\u7DDA<rt>\u3059\u3044\u3078\u3044\u305B\u3093</rt></ruby>\u306B<ruby>\u541B<rt>\u304D\u307F</rt></ruby>\u306F<ruby>\u6CA1<rt>\u307C\u3063</rt></ruby>\u3059\u308B\u306A\u304B\u308C</p>
+<p>\u308F\u308C\u3089\u306F<ruby>\u5C71\u5CB3<rt>\u3055\u3093\u304C\u304F</rt></ruby>\u306E<ruby>\u5CF0\u3005<rt>\u307F\u306D\u307F\u306D</rt></ruby>\u3068\u306A\u308A<br>
+<ruby>\u672A\u6765<rt>\u307F\u3089\u3044</rt></ruby>\u304B\u3089<ruby>\u5439<rt>\u3075</rt></ruby>\u304F<ruby>\u98A8<rt>\u304B\u305C</rt></ruby>\u306B<ruby>\u982D<rt>\u3053\u3046\u3079</rt></ruby>\u3092\u3042\u3052\u3088\u3046</p>
+<p><ruby>\u7D3A\u8272<rt>\u3053\u3093\u3044\u308D</rt></ruby>\u306E\u3046\u306D\u308A\u304C<br>
+\u306E\u307F\u3064\u304F\u3059<ruby>\u65E5<rt>\u3072</rt></ruby>\u304C<ruby>\u6765<rt>\u304D</rt></ruby>\u3066\u3082<br>
+<ruby>\u6C34\u5E73\u7DDA<rt>\u3059\u3044\u3078\u3044\u305B\u3093</rt></ruby>\u306B<ruby>\u541B<rt>\u304D\u307F</rt></ruby>\u306F<ruby>\u6CA1<rt>\u307C\u3063</rt></ruby>\u3059\u308B\u306A\u304B\u308C</p>
+<p><ruby>\u900F\u660E<rt>\u3068\u3046\u3081\u3044</rt></ruby>\u306A<ruby>\u5B87\u5B99<rt>\u3046\u3061\u3085\u3046</rt></ruby>\u306E<br>
+<ruby>\u98A8<rt>\u304B\u305C</rt></ruby>\u3068<ruby>\u5149<rt>\u3072\u304B\u308A</rt></ruby>\u3092<ruby>\u53D7<rt>\u3046</rt></ruby>\u3051\u3066<br>
+<ruby>\u5E83<rt>\u3072\u308D</rt></ruby>\u3044<ruby>\u4E16\u754C<rt>\u305B\u304B\u3044</rt></ruby>\u306B\u3000<ruby>\u6B63<rt>\u305F\u3060</rt></ruby>\u3057\u3044<ruby>\u6642\u4EE3<rt>\u3058\u3060\u3044</rt></ruby>\u3092\u3064\u304F\u308C</p>
+<p>\u308F\u308C\u3089\u306F\u305F\u3086\u307E\u306A\u304F<ruby>\u9032<rt>\u3059\u3059</rt></ruby>\u307F<ruby>\u7D9A<rt>\u3064\u3065</rt></ruby>\u3051\u3093<br>
+<ruby>\u672A\u6765<rt>\u307F\u3089\u3044</rt></ruby>\u304B\u3089<ruby>\u5439<rt>\u3075</rt></ruby>\u304F<ruby>\u98A8<rt>\u304B\u305C</rt></ruby>\u306B\u30BB\u30A4\u30EB\u3092\u3042\u3052\u3088\u3046</p>
+<p><ruby>\u7D3A\u8272<rt>\u3053\u3093\u3044\u308D</rt></ruby>\u306E\u3046\u306D\u308A\u304C<br>
+\u306E\u307F\u3064\u304F\u3059<ruby>\u65E5<rt>\u3072</rt></ruby>\u304C<ruby>\u6765<rt>\u304D</rt></ruby>\u3066\u3082<br>
+<ruby>\u6C34\u5E73\u7DDA<rt>\u3059\u3044\u3078\u3044\u305B\u3093</rt></ruby>\u306B<ruby>\u541B<rt>\u304D\u307F</rt></ruby>\u306F<ruby>\u6CA1<rt>\u307C\u3063</rt></ruby>\u3059\u308B\u306A\u304B\u308C</p>
+</div>
+</details>
+<p><strong>[A1]</strong></p>
+<p><leipzig-gloss lang="ja,en,en,zh" def="..:#" data="
+\u3053\u3093\u3044\u308D\u306E \u3046\u306D\u308A\u304C \u3000 \u306E\u307F \u3064\u304F\u3059 \u3072\u304C \u304D\u3066\u3082
+kon'iro-no uneri-ga \u3000 no-mi tsuku-su hi-ga ki-te-mo
+navy_blue-POS surge-SUB  devour-CONT exhaust-\u2205 day-SUB come.CONT-CONJ-also
+\u5373\u4F7F\u6DF1\u84DD\u8272\u7684\u957F\u6D6A\u541E\u566C\u4E00\u5207\u7684\u65E5\u5B50\u5230\u6765
+" /></p>
+<p><leipzig-gloss lang="ja,en,en,zh" def="..:#" data="
+\u3059\u3044\u3078\u3044\u305B\u3093\u306B \u304D\u307F\u306F \u307C\u3063\u3059\u308B \u306A\u304B\u308C
+suiheisen-ni kimi-wa bos-suru nakare
+horizon-LOC 2SG.HON-TOP sink-\u2205 must_not
+\u4F60\u4EA6\u6BCB\u6C89\u5165\u6D77\u5E73\u7EBF\u4E4B\u4E0B
+" /></p>
+<p><strong>[B1]</strong></p>
+<p><leipzig-gloss lang="ja,en,en,zh" def="..:#" data="
+\u308F\u308C\u3089\u306F \u3055\u3093\u304C\u304F\u306E \u307F\u306D\u307F\u306D\u3068 \u306A\u308A
+warera-wa sangaku-no mine~mine-to na-ri
+1PL-TOP mountain-POS peak~PL-COMP become-CONJ
+\u8BA9\u6211\u4EEC\u5316\u4F5C\u5C71\u4E4B\u7FA4\u5CF0
+" /></p>
+<p><leipzig-gloss lang="ja,en,en,zh" def="..:#" data="
+\u307F\u3089\u3044\u304B\u3089 \u3075\u304F \u304B\u305C\u306B \u3053\u3046\u3079\u3092 \u3042\u3052 \u3088\u3046
+mirai-kara fu-ku kaze-ni ko\u0304be-o age yo\u0304
+future-from blow-\u2205 wind-toward head-OBJ raise.CONJ VOL
+\u8FCE\u7740\u81EA\u672A\u6765\u5439\u6765\u7684\u98CE\uFF0C\u6602\u9996\u633A\u80F8
+" /></p>
+<p>[A1]\u2026</p>
+<p><strong>[A2]</strong></p>
+<p><leipzig-gloss lang="ja,en,en,zh" def="..:#" data="
+\u3068\u3046\u3081\u3044\u306A \u3046\u3061\u3085\u3046\u306E
+toumei-na uchu-no
+transparent-ADJZ universe-POS
+\u90A3\u900F\u660E\u5B87\u5B99\u7684
+" /></p>
+<p><leipzig-gloss lang="ja,en,en,zh" def="..:#" data="
+\u304B\u305C \u3068 \u3072\u304B\u308A\u3092 \u3046\u3051\u3066  
+kaze to hikari-o uke-te
+wind and light-OBJ receive.CONJ-CONT
+\u98CE\u4E0E\u5149\uFF0C\u6211\u4EEC\u63A5\u53D7\u7740\uFF0C
+" /></p>
+<p><leipzig-gloss lang="ja,en,en,zh" def="..:#" data="
+\u3072\u308D\u3044 \u305B\u304B\u3044\u306B \u3000 \u305F\u3060\u3057\u3044 \u3058\u3060\u3044\u3092 \u3064\u304F\u308C
+hiro-i sekai-ni  tadashi-i jidai-o tsuku-re
+vast-\u2205 word-in  upright-\u2205 era-OBJ make-IMP
+\u5728\u5E7F\u9614\u7684\u4E16\u754C\u4E2D\uFF0C\u521B\u9020\u6B63\u76F4\u7684\u65F6\u4EE3
+" /></p>
+<p><strong>[B2]</strong></p>
+<p><leipzig-gloss lang="ja,en,en,zh" def="..:#" data="
+\u308F\u308C\u3089\u306F \u305F\u3086\u307E\u306A\u304F \u3059\u3059\u307F \u3064\u3065\u3051\u3093 
+warera-wa tayu-ma-naku susu-mu tsuzu-ke-n
+1PL-TOP slacken-NPFV-NEG.ADVZ advance-\u2205 continue-IMP-VOL
+\u8BA9\u6211\u4EEC\u4E0D\u61C8\u524D\u8FDB\uFF0C\u4E00\u523B\u4E0D\u505C
+" /></p>
+<p><leipzig-gloss lang="ja,en,en,zh" def="..:#" data="
+\u307F\u3089\u3044\u304B\u3089 \u3075\u304F \u304B\u305C\u306B \u30BB\u30A4\u30EB\u3092 \u3042\u3052 \u3088\u3046
+mirai-kara fu-ku kaze-ni sairu-o age yo\u0304
+future-from blow-\u2205 wind-toward sail-OBJ raise.CONJ VOL
+\u8FCE\u7740\u81EA\u672A\u6765\u5439\u6765\u7684\u98CE\uFF0C\u626C\u8D77\u8239\u5E06
+" /></p>
+<p>[A1]\u2026</p>
+</div>`})}}};export{d as category,g as date,k as default,c as keywords,m as tags,l as title};
