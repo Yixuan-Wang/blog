ee591d65e679f827bf887392220119d7c274e1ab
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-04-01 09:53:42 +0000

    deploy: ab8122ebea6296e43f8db88817bc0a7ed805134e

---
+++ assets/about-en.19e427d7.js
@@ -0,0 +1,11 @@
+import{_ as t}from"./TheArticle.cf9ef9ce.js";import{o as a,e as o}from"./vendor.68320762.js";const i={setup(r,{expose:n}){const e={};return n({frontmatter:e}),(l,c)=>{const s=t;return a(),o(s,{frontmatter:e,inner:`<div><h2 id="naming" tabindex="-1">Naming</h2>
+<p>The Chinese character of <span :lang="zh-Hans">\u535A</span>(wide, abundant, phono-semantical translation of \u2018blog\u2019; /po/) was reconstructed as [p\u0251k] in Middle Chinese. Among which, the open back unrounded vowel \u2018\u0251\u2019 (U+0251 <span style="font-variant: all-small-caps;">LATIN SMALL LETTER ALPHA</span>) is poorly supported in fonts, so a simpler transliteration \u2018Pak\u2019 is used.</p>
+<p>Besides, talking about the motto \u2018This is a bl\u043Eg, not a bl\u03BFg\u2019, the first \u2018\u043E\u2019(U+043E <span style="font-variant: all-small-caps;">CYRILLIC SMALL LETTER O</span>) is not the same character as the second \u2018o\u2019(U+006F <span style="font-variant: all-small-caps;">LATIN SMALL LETTER O</span>)\u{1F61C}.</p>
+<h2 id="website" tabindex="-1">Website</h2>
+<p>This blog is automatically built by GitHub Actions. It is hosted on my own cloud server.</p>
+<h2 id="licensing" tabindex="-1">Licensing</h2>
+<p>The code is based on Vue and Vite\u26A1, bootstrapped by Vitesse template. The codebase is licensed under MIT license.</p>
+<p>For contents, the following license applies:</p>
+<p><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
+This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.</p>
+</div>`})}}};var m=Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:i});export{m as _};
