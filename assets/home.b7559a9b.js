8fdd2a1230ee332cbb59328154bc03fcde80a6d8
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-04-05 06:16:59 +0000

    deploy: af46c2e5f7473a9a943837c7f89e8a15137e828e

--- assets/home.b7559a9b.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./app.dbede0b5.js";import{r,o as t,b as c,i as s}from"./vendor.a2cf0b24.js";const n={},a={class:"mb-8"};function _(m,i){const e=r("router-view");return t(),c("main",a,[s(e)])}var f=o(n,[["render",_]]);export{f as default};
