0fd729c4dd1067a90a735f1e0559fedc9f673f5c
Yixuan-Wang <21151119+Yixuan-Wang@users.noreply.github.com>
2022-05-16 18:46:28 +0800

    Revert "feat: add chromium bug notice"
    This reverts commit bf5832365c2e021adb89b9ae299c68baa8d2415a.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,7 +11,7 @@ useHead({
 
 const store = useStore()
 
-const { t, locale } = useI18n()
+const { t } = useI18n()
 const selection = reactive<Record<string, boolean>>({
   posts: true,
   sheets: true,
@@ -23,14 +23,6 @@ const selectionOthers = reactive<Record<string, boolean>>({
 
 const filter = computed(() => (a: Article) => selection?.[a.genre as keyof typeof selection])
 const articles = computed(() => store.articles.filter(filter.value))
-
-const chromiumBugNotice = ref(false)
-const chromiumVersion = ref('')
-if (!import.meta.env.SSR) {
-  chromiumBugNotice.value = /Chrome/.test(navigator.userAgent)
-  if (chromiumBugNotice.value)
-    chromiumVersion.value = navigator.userAgent.match(/Chrome\/(.+?)(\s|$)/)![1]
-}
 </script>
 
 <template>
@@ -41,21 +33,6 @@ if (!import.meta.env.SSR) {
     This is a blоg, not a blοg.
   </p>
   <div class="flex flex-col gap-8">
-    <ClientOnly>
-      <div
-        v-if="chromiumBugNotice && locale === 'zh-Hans'"
-        class="rounded bg-acc text-bgd p-3"
-      >
-        <h2 class="text-xl">
-          <mdi-bug />
-          <span class="font-bold pl-1">不是你的锅，也不是我的锅</span>
-        </h2>
-        <p class="m-2">
-          你似乎在使用 <mdi-google-chrome /> Chromium 系浏览器，版本 {{ chromiumVersion }}。<br />
-          我们观察到 100 版本的 Chromium 在<strong>过渡动效渲染</strong>方面似乎存在问题，请刷新以解决颜色错误的问题。若影响阅读，请考虑使用 Firefox 或 Safari。
-        </p>
-      </div>
-    </ClientOnly>
     <div
       class="flex gap-2"
     >
