9c24308fd21b03a92d9552fd094aa0164c770ee6
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-07-04 17:12:06 +0000

    deploy: f789622f9640c0e2b6b1cbb57371136ea95f83c1

---
+++ assets/jpn-lyrics-gloss-c.a20ec0d9.js
@@ -0,0 +1,179 @@
+import{_ as a}from"./TheArticle.f307d263.js";import{o as e,e as u}from"./vendor.7f9880fc.js";import"./app.dc9a85c0.js";const p="\u65E5\u8BED\u6B4C\u8BCD\u6807\u6CE8\u4E4B\u4E19",g="2022-02-18T00:00:00.000Z",d="misc",h=["lyrics"],m=["lang:jpn","anime"],c={setup(b,{expose:n}){const r={title:"\u65E5\u8BED\u6B4C\u8BCD\u6807\u6CE8\u4E4B\u4E19",date:"2022-02-18T00:00:00.000Z",category:"misc",tags:["lyrics"],keywords:["lang:jpn","anime"]};return n({frontmatter:r}),(y,s)=>{const t=a;return e(),u(t,{frontmatter:r,inner:`<div><p>\u8BF6\uFF1F\u662F\u4E8C\u6B21\u5143\uFF1F</p>
+<!-- more -->
+<h2 id="\u6B8B\u9177\u306A\u5929\u4F7F\u306E\u30C6\u30FC\u30BC" tabindex="-1">\u6B8B\u9177\u306A\u5929\u4F7F\u306E\u30C6\u30FC\u30BC</h2>
+<p><span class="bg-clip-text text-transparent bg-gradient-to-r from-[#7f3da0] to-[#89ff69]">\u300A\u65B0\u4E16\u7EAA\u798F\u97F3\u6218\u58EB\u300B</span> TV\u7248 OP<br>
+\u53CA\u5DDD\u7720\u5B50\uFF08\u8BCD\uFF09<br>
+\u4F50\u85E4\u82F1\u654F\uFF08\u66F2\uFF09</p>
+<details><summary>\u6B4C\u8BCD</summary>
+<div class="py-4 %" lang="ja">
+<p><ruby>\u6B8B\u9177<rt>\u3056\u3093\u3053\u304F</rt></ruby>\u306A<ruby>\u5929\u4F7F<rt>\u3066\u3093\u3057</rt></ruby>\u306E\u3088\u3046\u306B\u3000<ruby>\u5C11\u5E74<rt>\u3057\u3087\u3046\u306D\u3093</rt></ruby>\u3088<ruby>\u795E\u8A71<rt>\u3057\u3093\u308F</rt></ruby>\u306B\u306A\u308C</p>
+<p><ruby>\u84BC<rt>\u304B\u305C</rt></ruby>\u304C\u3044\u307E\u3000<ruby>\u80F8<rt>\u3080\u306D</rt></ruby>\u306E\u30C9\u30A2\u3092<ruby>\u53E9<rt>\u305F\u305F</rt></ruby>\u3044\u3066\u3082<br>
+<ruby>\u79C1<rt>\u308F\u305F\u3057</rt></ruby>\u3060\u3051\u3092\u305F\u3060<ruby>\u898B<rt>\u307F</rt></ruby>\u3064\u3081\u3066\u3000<ruby>\u5FAE\u7B11<rt>\u307B\u307B\u3048</rt></ruby>\u3093\u3067\u308B\u3042\u306A\u305F<br>
+\u305D\u3063\u3068\u3075\u308C\u308B\u3082\u306E\u3000\u3082\u3068\u3081\u308B\u3053\u3068\u306B<ruby>\u5922\u4E2D<rt>\u3080\u3061\u3085\u3046</rt></ruby>\u3067<br>
+<ruby>\u904B\u547D<rt>\u3046\u3093\u3081\u3044</rt></ruby>\u3055\u3048\u307E\u3060<ruby>\u77E5<rt>\u3057</rt></ruby>\u3089\u306A\u3044\u3000\u3044\u305F\u3044\u3051\u306A<ruby>\u77B3<rt>\u3072\u3068\u307F</rt></ruby></p>
+<p>\u3060\u3051\u3069\u3044\u3064\u304B<ruby>\u6C17\u4ED8<rt>\u304D\u3065</rt></ruby>\u304F\u3067\u3057\u3087\u3046\u3000\u305D\u306E<ruby>\u80CC\u4E2D<rt>\u305B\u306A\u304B</rt></ruby>\u306B\u306F<br>
+<ruby>\u9065<rt>\u306F\u308B</rt></ruby>\u304B<ruby>\u672A\u6765<rt>\u307F\u3089\u3044</rt></ruby>\u3081\u3056\u3059\u305F\u3081\u306E<ruby>\u7FBD\u6839<rt>\u306F\u306D</rt></ruby>\u304C\u3042\u308B\u3053\u3068</p>
+<p><ruby>\u6B8B\u9177<rt>\u3056\u3093\u3053\u304F</rt></ruby>\u306A<ruby>\u5929\u4F7F<rt>\u3066\u3093\u3057</rt></ruby>\u306E\u30C6\u30FC\u30BC\u3000<ruby>\u7A93<rt>\u307E\u3069</rt></ruby><ruby>\u8FBA<rt>\u3079</rt></ruby>\u304B\u3089\u3084\u304C\u3066<ruby>\u98DB<rt>\u3068</rt></ruby>\u3073<ruby>\u7ACB<rt>\u305F</rt></ruby>\u3064<br>
+\u307B\u3068\u3070\u3057\u308B<ruby>\u71B1<rt>\u3042\u3064</rt></ruby>\u3044\u30D1\u30C8\u30B9\u3067\u3000<ruby>\u601D<rt>\u304A\u3082</rt></ruby>\u3044<ruby>\u51FA<rt>\u3067</rt></ruby>\u3092<ruby>\u88CF\u5207<rt>\u3046\u3089\u304E</rt></ruby>\u308B\u306A\u3089<br>
+\u3053\u306E<ruby>\u5B87\u5B99<rt>\u305D\u3089</rt></ruby>\u3092<ruby>\u62B1<rt>\u3060</rt></ruby>\u3044\u3066<ruby>\u8F1D<rt>\u304B\u304C\u3084</rt></ruby>\u304F<br>
+<ruby>\u5C11\u5E74<rt>\u3057\u3087\u3046\u306D\u3093</rt></ruby>\u3088<ruby>\u795E\u8A71<rt>\u3057\u3093\u308F</rt></ruby>\u306B\u306A\u308C</p>
+<p>\u305A\u3063\u3068<ruby>\u7720<rt>\u306D\u3080</rt></ruby>\u3063\u3066\u308B\u3000<ruby>\u79C1<rt>\u308F\u305F\u3057</rt></ruby>\u306E<ruby>\u611B<rt>\u3042\u3044</rt></ruby>\u306E<ruby>\u63FA<rt>\u3086</rt></ruby>\u308A\u304B\u3054<br>
+\u3042\u306A\u305F\u3060\u3051\u304C\u3000<ruby>\u5922<rt>\u3086\u3081</rt></ruby>\u306E<ruby>\u4F7F\u8005<rt>\u3057\u3057\u3083</rt></ruby>\u306B<ruby>\u547C<rt>\u3088</rt></ruby>\u3070\u308C\u308B<ruby>\u671D<rt>\u3042\u3055</rt></ruby>\u304C<ruby>\u6765<rt>\u304F</rt></ruby>\u308B<br>
+<ruby>\u7D30<rt>\u307B\u305D</rt></ruby>\u3044<ruby>\u9996\u7B4B<rt>\u304F\u3073\u3059\u3058</rt></ruby>\u3092\u3000<ruby>\u6708<rt>\u3064\u304D</rt></ruby>\u3042\u304B\u308A\u304C<ruby>\u6620<rt>\u3046\u3064</rt></ruby>\u3057\u3066\u308B<br>
+<ruby>\u4E16\u754C\u4E2D<rt>\u305B\u304B\u3044\u3058\u3085\u3046</rt></ruby>\u306E<ruby>\u6642<rt>\u3068\u304D</rt></ruby>\u3092<ruby>\u6B62<rt>\u3068</rt></ruby>\u3081\u3066\u3000<ruby>\u9589<rt>\u3068</rt></ruby>\u3058\u3053\u3081\u305F\u3044\u3051\u3069</p>
+<p>\u3082\u3057\u3082\u3075\u305F\u308A<ruby>\u9022<rt>\u3042</rt></ruby>\u3048\u305F\u3053\u3068\u306B<ruby>\u610F\u5473<rt>\u3044\u307F</rt></ruby>\u304C\u3042\u308B\u306A\u3089<br>
+<ruby>\u79C1<rt>\u308F\u305F\u3057</rt></ruby>\u306F\u305D\u3046<ruby>\u81EA\u7531<rt>\u3058\u3086\u3046</rt></ruby>\u3092<ruby>\u77E5<rt>\u3057</rt></ruby>\u308B\u305F\u3081\u306E\u30D0\u30A4\u30D6\u30EB</p>
+<p><ruby>\u6B8B\u9177<rt>\u3056\u3093\u3053\u304F</rt></ruby>\u306A<ruby>\u5929\u4F7F<rt>\u3066\u3093\u3057</rt></ruby>\u306E\u30C6\u30FC\u30BC\u3000<ruby>\u60B2<rt>\u304B\u306A</rt></ruby>\u3057\u307F\u304C\u305D\u3057\u3066\u306F\u3058\u307E\u308B<br>
+<ruby>\u62B1<rt>\u3060</rt></ruby>\u304D\u3057\u3081\u305F<ruby>\u547D<rt>\u3044\u306E\u3061</rt></ruby>\u306E\u304B\u305F\u3061\u3000\u305D\u306E<ruby>\u5922<rt>\u3086\u3081</rt></ruby>\u306B<ruby>\u76EE\u899A<rt>\u3081\u3056</rt></ruby>\u3081\u305F\u3068\u304D<br>
+<ruby>\u8AB0<rt>\u3060\u308C</rt></ruby>\u3088\u308A\u3082<ruby>\u5149<rt>\u3072\u304B\u308A</rt></ruby>\u3092<ruby>\u653E<rt>\u306F\u306A</rt></ruby>\u3064<br>
+<ruby>\u5C11\u5E74<rt>\u3057\u3087\u3046\u306D\u3093</rt></ruby>\u3088<ruby>\u795E\u8A71<rt>\u3057\u3093\u308F</rt></ruby>\u306B\u306A\u308C</p>
+<p><ruby>\u4EBA<rt>\u3072\u3068</rt></ruby>\u306F<ruby>\u611B<rt>\u3042\u3044</rt></ruby>\u3092\u3064\u3080\u304E\u306A\u304C\u3089<ruby>\u6B74\u53F2<rt>\u308C\u304D\u3057</rt></ruby>\u3092\u3064\u304F\u308B<br>
+<ruby>\u5973\u795E<rt>\u3081\u304C\u307F</rt></ruby>\u306A\u3093\u3066\u306A\u308C\u306A\u3044\u307E\u307E<ruby>\u79C1<rt>\u308F\u305F\u3057</rt></ruby>\u306F<ruby>\u751F<rt>\u3044</rt></ruby>\u304D\u308B</p>
+<p><ruby>\u6B8B\u9177<rt>\u3056\u3093\u3053\u304F</rt></ruby>\u306A<ruby>\u5929\u4F7F<rt>\u3066\u3093\u3057</rt></ruby>\u306E\u30C6\u30FC\u30BC\u3000<ruby>\u7A93<rt>\u307E\u3069</rt></ruby><ruby>\u8FBA<rt>\u3079</rt></ruby>\u304B\u3089\u3084\u304C\u3066<ruby>\u98DB<rt>\u3068</rt></ruby>\u3073<ruby>\u7ACB<rt>\u305F</rt></ruby>\u3064<br>
+\u307B\u3068\u3070\u3057\u308B<ruby>\u71B1<rt>\u3042\u3064</rt></ruby>\u3044\u30D1\u30C8\u30B9\u3067\u3000<ruby>\u601D<rt>\u304A\u3082</rt></ruby>\u3044<ruby>\u51FA<rt>\u3067</rt></ruby>\u3092<ruby>\u88CF\u5207<rt>\u3046\u3089\u304E</rt></ruby>\u308B\u306A\u3089<br>
+\u3053\u306E<ruby>\u5B87\u5B99<rt>\u305D\u3089</rt></ruby>\u3092<ruby>\u62B1<rt>\u3060</rt></ruby>\u3044\u3066<ruby>\u8F1D<rt>\u304B\u304C\u3084</rt></ruby>\u304F<br>
+<ruby>\u5C11\u5E74<rt>\u3057\u3087\u3046\u306D\u3093</rt></ruby>\u3088<ruby>\u795E\u8A71<rt>\u3057\u3093\u308F</rt></ruby>\u306B\u306A\u308C</p>
+</div>
+</details>
+<div class="%">
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3056\u3093\u3053\u304F\u306A \u3066\u3093\u3057\u306E \u3088\u3046\u306B \u3000 \u3057\u3087\u3046\u306D\u3093 \u3088 \u3057\u3093\u308F\u306B \u306A\u308C
+zankoku-na tenshi-no yo\u0304ni  shonen yo shinwa-ni na-re
+cruel-ADJZ angel-POS like \u3000 teen VOC legendary-to become-IMP
+\u5C31\u50CF\u90A3\u6B8B\u9177\u7684\u5929\u4F7F\uFF0C\u5C11\u5E74\u554A\uFF0C\u6210\u4E3A\u795E\u8BDD\u5427
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3042\u304A\u3044 \u304B\u305C\u304C \u3044\u307E \u3000 \u3080\u306D\u306E \u30C9\u30A2\u3092 \u305F\u305F\u3044\u3066\u3082
+ao-i kaze-ga ima \u3000 mune-no doa-o tata-i-te-mo
+blue-\u2205 wind-SBJ now \u3000 heart-POS door-OBJ knock-CONT-CONJ-also
+\u6B64\u523B\uFF0C\u5C3D\u7BA1\u82CD\u84DD\u8272\u7684\u98CE\u53E9\u51FB\u7740\u6211\u7684\u5FC3\u95E8
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u308F\u305F\u3057 \u3060\u3051\u3092 \u305F\u3060 \u307F\u3064\u3081\u3066 \u3000 \u307B\u307B\u3048\u3093\u3067\u308B \u3042\u306A\u305F
+watashi dake-o tada mitsume-te \u3000 hohoe-nde-ru anata
+1SG only-OBJ but stare_at.CONT-CONJ \u3000 smile-CONT.CONJ-PRS.PROG 2SG
+\u4F46\u4F60\uFF0C\u53EA\u662F\u51DD\u89C6\u7740\u6211\uFF0C\u5FAE\u7B11\u7740
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u305D\u3063\u3068 \u3075\u308C\u308B \u3082\u306E \u3000 \u3082\u3068\u3081\u308B \u3053\u3068\u306B \u3080\u3061\u3085\u3046\u3067  
+sotto fure-ru mono  motome-ru kotoni muchu\u0304-de
+gently touch-\u2205 because  desire.ACT-\u2205 particularly engross-CONT.CONJ
+\u56E0\u4E3A\u8F7B\u8F7B\u7684\u4E00\u89E6\uFF0C\u6E34\u671B\u4E4B\u60C5\u66F4\u4E3A\u75F4\u72C2
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3046\u3093\u3081\u3044 \u3055\u3048 \u307E\u3060 \u3057\u3089\u306A\u3044 \u3000 \u3044\u305F\u3044\u3051\u306A \u3072\u3068\u307F
+unmei sae mada shi-ranai  itaike-na hitomi
+destiny even yet know-NEG  tender-ADJZ pupil
+\u8FDE\u547D\u8FD0\u4E5F\u5C1A\u4E0D\u77E5\u6653\u7684\u67D4\u60C5\u7684\u773C\u7738
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3060\u3051\u3069 \u3044\u3064\u304B \u304D\u3065\u304F\u3067\u3057\u3087\u3046 \u3000 \u305D\u306E \u305B\u306A\u304B\u306B\u306F 
+dakedo itsuka kitsuku-desho\u0304  sono senaka-ni-wa
+however one_day notice-SUPP  DEI.MED back-LOC-TOP
+\u4F46\u6709\u4E00\u5929\uFF0C\u4F60\u4F1A\u53D1\u89C9\u80CC\u4E0A
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u306F\u308B\u304B \u307F\u3089\u3044 \u3081\u3056\u3059 \u305F\u3081\u306E \u306F\u306D\u304C \u3042\u308B \u3053\u3068
+haruka mirai meza-su tame-no hane-ga a-ru koto
+distant future head.towards-\u2205 sake-POS wing-SBJ exist-\u2205 NZ
+\u6709\u4E3A\u524D\u5F80\u9065\u8FDC\u672A\u6765\u800C\u751F\u7684\u7FBD\u7FFC
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3056\u3093\u3053\u304F\u306A \u3066\u3093\u3057\u306E \u30C6\u30FC\u30BC \u3000 \u307E\u3069\u3079 \u304B\u3089 \u3084\u304C\u3066 \u3068\u3073\u305F\u3064
+zankoku-na tenshi-no te\u0304ze  madobe kara yagate to-bi-ta-tsu
+cruel-ADJZ angel-POS thesis  window_side from soon fly-CONT-stand_up-\u2205 
+\u6B8B\u9177\u5929\u4F7F\u7684\u884C\u52A8\u7EB2\u9886\uFF0C\u65CB\u5373\u4ECE\u7A97\u8FB9\u98DE\u51FA
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u307B\u3068\u3070\u3057\u308B \u3042\u3064\u3044 \u30D1\u30C8\u30B9\u3067 \u3000 \u304A\u3082\u3044\u3067\u3092 \u3046\u3089\u304E\u308B \u306A\u3089 
+hotobashi-ru atsu-i patosu-de  omoide-o uragi-ru nara
+surge-\u2205 passionate-\u2205 pathos-INS  memories-OBJ betray-\u2205 if
+\u82E5\u51ED\u90A3\u8FF8\u53D1\u7684\u70BD\u70C8\u60C5\u611F\uFF0C\u5C06\u56DE\u5FC6\u80CC\u53DB
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3053\u306E \u305D\u3089\u3092 \u3060\u3044\u3066 \u304B\u304C\u3084\u304F 
+kono sora-o da-i-te kagaya-ku
+DEI.PROX universe-OBJ embrace-CONT-CONJ shine-\u2205
+\u62E5\u62B1\u8FD9\u5B87\u5B99\uFF0C\u653E\u5C04\u51FA\u5149\u8292
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3057\u3087\u3046\u306D\u3093 \u3088 \u3057\u3093\u308F\u306B \u306A\u308C
+shonen yo shinwa-ni na-re
+teen VOC legendary-to become-IMP
+\u5C11\u5E74\u554A\uFF0C\u6210\u4E3A\u795E\u8BDD\u5427
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u305A\u3063\u3068 \u306D\u3080\u3063\u3066\u308B \u3000 \u308F\u305F\u3057\u306E \u3042\u3044\u306E \u3086\u308A\u304B\u3054
+zutto nemu-tte-ru  watashi-no ai-no yurikago
+always sleep-CONT.CONJ-PRS.PROG  1SG-POS love-POS cradle
+\u4E00\u76F4\u6C89\u7761\u7740\u7684\uFF0C\u6211\u7684\u7231\u4E4B\u6447\u7BEE
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3042\u306A\u305F \u3060\u3051\u304C \u3000 \u3086\u3081\u306E \u3057\u3057\u3083\u306B \u3088\u3070\u308C\u308B \u3042\u3055\u304C \u304F\u308B
+anata dake-ga  yume-no shisha-ni yo-bareru asa-ga ku-ru
+2SG only-SBJ  dream-POS messenger-by call-PASS morning-SBJ come-\u2205
+\u6E05\u6668\u964D\u4E34\uFF0C\u68A6\u4E4B\u4F7F\u8005\u60DF\u547C\u5524\u4F60\u4E00\u4EBA
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u307B\u305D\u3044 \u304F\u3073\u3059\u3058\u3092 \u3000 \u3064\u304D\u3042\u304B\u308A\u304C \u3046\u3064\u3057\u3066\u308B  
+hoso-i kubisuji-o  tsukiakari-ga utsu-shi-te-ru
+slender-\u2205 nape-OBJ  moonlight-SBJ cast-CONT-CONJ-PRS.PROG
+\u6708\u5149\u6620\u7740\u4F60\u7EA4\u7EC6\u7684\u8116\u9888
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u305B\u304B\u3044\u3058\u3085\u3046\u306E \u3068\u304D\u3092 \u3068\u3081\u3066 \u3000 \u3068\u3058\u3053\u3081\u305F\u3044 \u3051\u3069
+sekaiju\u0304-no toki-o tome-te  tojikome-tai kedo
+all_the_world-POS time-OBJ stop.CONT-CONJ  confine.ACT-DES but
+\u6211\u591A\u4E48\u60F3\u505C\u4E0B\u5168\u4E16\u754C\u7684\u65F6\u95F4\uFF0C\u5C06\u5B83\u5C01\u5B58
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3082\u3057\u3082 \u3075\u305F\u308A \u3042\u3048\u305F \u3053\u3068\u306B \u3044\u307F\u304C \u3042\u308B \u306A\u3089
+moshimo futari a-e-ta koto-ni imi-ga a-ru nara
+even_if two_persons meet-PFV-PST NZ-LOC meaning-SUB exist-\u2205 if
+\u5982\u679C\u8BF4\u6211\u4EEC\u4E8C\u4EBA\u7684\u76F8\u4F1A\u5B58\u5728\u7740\u67D0\u79CD\u610F\u4E49
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u308F\u305F\u3057\u306F \u305D\u3046 \u3058\u3086\u3046\u3092 \u3057\u308B \u305F\u3081\u306E \u30D0\u30A4\u30D6\u30EB
+watashi-wa so\u0304 jiyu\u0304-o shi-ru tame-no baiburu
+1SG-TOP such freedom-OBJ know-\u2205 sake-POS babel
+\u90A3\u5B83\u5C31\u662F\u542F\u8FEA\u6211\u4F55\u4E3A\u81EA\u7531\u7684\u5723\u7ECF
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3056\u3093\u3053\u304F\u306A \u3066\u3093\u3057\u306E \u30C6\u30FC\u30BC \u3000 \u304B\u306A\u3057\u307F\u304C \u305D\u3057\u3066 \u306F\u3058\u307E\u308B
+zankoku-na tenshi-no te\u0304ze  kanashimi-ga soshite hajima-ru
+cruel-ADJZ angel-POS thesis  sadness-SUB in_that_way begin.MID-\u2205
+\u6B8B\u9177\u5929\u4F7F\u7684\u884C\u52A8\u7EB2\u9886\uFF0C\u60B2\u4F24\u7531\u6B64\u5F00\u59CB
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3060\u304D \u3057\u3081\u305F \u3044\u306E\u3061\u306E \u304B\u305F\u3061 \u3000 \u305D\u306E \u3086\u3081\u306B \u3081\u3056\u3081\u305F \u3068\u304D
+da-ki shime-ta inochi-no katachi  sono yume-ni mezame-ta toki
+embrace-CONT close.ACT-PST life-POS form  DEI.MD dream-from awake.ACT-PST time
+\u7D27\u7D27\u76F8\u62E5\u7684\u751F\u547D\u4E4B\u5F62\uFF0C\u5728\u4ECE\u90A3\u68A6\u4E2D\u9192\u6765\u4E4B\u65F6
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3060\u308C \u3088\u308A\u3082 \u3072\u304B\u308A\u3092 \u306F\u306A\u3064
+dare yori-mo hikari-o hana-tsu
+who than-EMP light-OBJ emit-\u2205
+\u653E\u5C04\u51FA\u6700\u8000\u773C\u7684\u5149\u8292
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3057\u3087\u3046\u306D\u3093 \u3088 \u3057\u3093\u308F\u306B \u306A\u308C
+shonen yo shinwa-ni na-re
+teen VOC legendary-to become-IMP
+\u5C11\u5E74\u554A\uFF0C\u6210\u4E3A\u795E\u8BDD\u5427
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3072\u3068\u306F \u3042\u3044\u3092 \u3064\u3080\u304E \u306A\u304C\u3089 \u308C\u304D\u3057\u3092 \u3064\u304F\u308B  
+hito-wa ai-o tsumu-gi nagara rekishi-o tsuku-ru
+humankind-TOP love-OBJ spin_yarn-CONT while history-OBJ make-\u2205 
+\u4EBA\u7C7B\u4E00\u8FB9\u7F16\u7EC7\u7231\u60C5\uFF0C\u4E00\u8FB9\u4E66\u5199\u5386\u53F2
+"/></p>
+<p><leipzig-gloss def="..:#" lang="ja,en,en,zh-Hans" data="
+\u3081\u304C\u307F \u306A\u3093\u3066 \u306A\u308C\u306A\u3044 \u307E\u307E \u3000 \u308F\u305F\u3057\u306F \u3044\u304D\u308B  
+nagami nante na-re-nai mama  watashi-wa iki-ru
+godess whatever become-HYP-NEG still  1SG-TOP live-\u2205
+\u4F9D\u7136\u65E0\u6CD5\u6210\u4E3A\u5973\u795E\u7684\u6211\uFF0C\u5C31\u8FD9\u6837\u751F\u5B58\u7740
+"/></p>
+<p>[\u91CD\u590D\u526F\u6B4C 1]</p>
+</div>
+</div>`})}}};export{d as category,g as date,c as default,m as keywords,h as tags,p as title};
