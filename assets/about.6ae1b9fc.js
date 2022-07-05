52fbf06715fc37e04b2ef9c897f00a2fc27a350f
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-07-05 14:11:47 +0000

    deploy: dd9d4894cfb1be935c582f89853e7bfcc5216d97

---
+++ assets/about.6ae1b9fc.js
@@ -0,0 +1,1 @@
+import{d as t,u as s,o as a,c as o,a as e,b as c,e as n,F as r}from"./app.0ea3237f.js";import m from"./about.edb975b8.js";import"./TheArticle.fdccdf20.js";const u=e("h1",{class:"mt-4 mb-8"}," \u5173\u4E8E ",-1),_={id:"md"},h=t({setup(i){return s({title:"\u5173\u4E8E | Pak"}),(d,p)=>(a(),o(r,null,[u,e("section",_,[c(n(m))])],64))}});export{h as default};
