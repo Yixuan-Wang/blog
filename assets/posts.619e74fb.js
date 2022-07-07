4c751f9cef8a04ab9ff81f03b1903bd265b0f1f0
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-07-07 15:28:03 +0000

    deploy: 347a6b0bf3d923bdc5b2b2597d931d13e274f2c8

--- assets/posts.619e74fb.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./TheTaxonomy.d8a6ef72.js";import{d as s,u as a,o as c,f as r}from"./app.141e6122.js";const i=s({setup(m){const o=e=>e.genre==="posts";return a({title:"\u6587\u7AE0 | Pak",meta:[{name:"description",content:"Posts on Pak"}]}),(e,p)=>{const t=n;return c(),r(t,{name:"\u6587\u7AE0",filter:o})}}});export{i as default};
