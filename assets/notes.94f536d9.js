2bb2964bd7e7b7184a245839565dfc007550bded
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-07-08 05:00:45 +0000

    deploy: 07ed1967a57a3099893b1a2b447cdef915abb29e

---
+++ assets/notes.94f536d9.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./TheTaxonomy.fe5141f1.js";import{d as a,u as s,o as c,f as r}from"./app.751e311b.js";const i=a({setup(m){const o=e=>e.genre==="notes";return s({title:"\u7B14\u8BB0 | Pak",meta:[{name:"description",content:"Notes on Pak"}]}),(e,_)=>{const t=n;return c(),r(t,{name:"\u7B14\u8BB0",filter:o})}}});export{i as default};
---
+++ assets/posts.12577699.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./TheTaxonomy.fe5141f1.js";import{d as s,u as a,o as c,f as r}from"./app.751e311b.js";const i=s({setup(m){const o=e=>e.genre==="posts";return a({title:"\u6587\u7AE0 | Pak",meta:[{name:"description",content:"Posts on Pak"}]}),(e,p)=>{const t=n;return c(),r(t,{name:"\u6587\u7AE0",filter:o})}}});export{i as default};
