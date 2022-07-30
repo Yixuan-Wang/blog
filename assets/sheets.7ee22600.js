2eb40670aef3bbd5d9a00439cd04dcf27045ea46
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-07-30 09:19:36 +0000

    deploy: bcb7dc1777a10c30032602773d36e063028ed1b3

---
+++ assets/sheets.7ee22600.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./TheTaxonomy.6201e4e8.js";import{d as a,u as s,o as c,f as r}from"./app.d120931a.js";const i=a({setup(m){const t=e=>e.genre==="sheets";return s({title:"\u6E05\u5355 | Pak",meta:[{name:"description",content:"Sheets on Pak"}]}),(e,_)=>{const o=n;return c(),r(o,{name:"\u6E05\u5355",filter:t})}}});export{i as default};
