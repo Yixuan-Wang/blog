d449690afc7a61be3168b49c8f42c1f2bc4997e1
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-07-30 07:47:02 +0000

    deploy: bcb7dc1777a10c30032602773d36e063028ed1b3

--- assets/posts.06361964.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./TheTaxonomy.ff8db89a.js";import{d as s,u as a,o as c,f as r}from"./app.7819cb04.js";const i=s({setup(m){const o=e=>e.genre==="posts";return a({title:"\u6587\u7AE0 | Pak",meta:[{name:"description",content:"Posts on Pak"}]}),(e,p)=>{const t=n;return c(),r(t,{name:"\u6587\u7AE0",filter:o})}}});export{i as default};
---
+++ assets/rust-magics.b235755f.js
@@ -0,0 +1,3 @@
+import{_ as r}from"./TheArticle.8f9cde5d.js";import{o as s,f as n}from"./app.c736d4e5.js";const _="In Rust We Thrust",i="2022-07-30T00:00:00.000Z",f="comp",l=["rust","pl"],d={setup(c,{expose:o}){const t={title:"In Rust We Thrust",date:"2022-07-30T00:00:00.000Z",category:"comp",tags:["rust","pl"]};return o({frontmatter:t}),(a,p)=>{const e=r;return s(),n(e,{frontmatter:t,inner:`<div><p>Rust \u8BED\u8A00\u7684\u4E00\u4E9B\u5947\u6280\u6DEB\u5DE7\u3002</p>
+<!-- more -->
+</div>`})}}};export{f as category,i as date,d as default,l as tags,_ as title};
