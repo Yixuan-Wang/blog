95e90ace33e877c79a0f06e291799478f0e74f69
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-07-30 16:52:58 +0000

    deploy: bcb7dc1777a10c30032602773d36e063028ed1b3

--- assets/posts.d1d7bd02.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./TheTaxonomy.6201e4e8.js";import{d as s,u as a,o as c,f as r}from"./app.d120931a.js";const i=s({setup(m){const o=e=>e.genre==="posts";return a({title:"\u6587\u7AE0 | Pak",meta:[{name:"description",content:"Posts on Pak"}]}),(e,p)=>{const t=n;return c(),r(t,{name:"\u6587\u7AE0",filter:o})}}});export{i as default};
---
+++ assets/sheets.9c7988ac.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./TheTaxonomy.f06e428d.js";import{d as a,u as s,o as c,f as r}from"./app.9c17e822.js";const i=a({setup(m){const t=e=>e.genre==="sheets";return s({title:"\u6E05\u5355 | Pak",meta:[{name:"description",content:"Sheets on Pak"}]}),(e,_)=>{const o=n;return c(),r(o,{name:"\u6E05\u5355",filter:t})}}});export{i as default};
