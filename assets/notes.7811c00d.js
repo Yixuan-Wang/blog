f90bc7f41a6e4801710b135f8a1318c8615094f5
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-09-12 09:12:32 +0000

    deploy: 097136aca3ecff46f002409d60ec6798a5936002

---
+++ assets/notes.7811c00d.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./TheTaxonomy.1022c281.js";import{d as a,f as s,o as c,j as r}from"./app.ee28ecff.js";const u=a({setup(m){const o=e=>e.genre==="notes";return s({title:"\u7B14\u8BB0 | Pak",meta:[{name:"description",content:"Notes on Pak"}]}),(e,_)=>{const t=n;return c(),r(t,{name:"\u7B14\u8BB0",filter:o})}}});export{u as default};
