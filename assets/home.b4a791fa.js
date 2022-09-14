b0bbc90b288678e2e1a79e53450492deed45fce4
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2022-09-14 17:03:48 +0000

    deploy: 6b1cf9cc64c1bd613ea9493fa5133dc53c9445ba

---
+++ assets/home.b4a791fa.js
@@ -0,0 +1,1 @@
+import{U as o,J as r,o as t,b as c,h as n}from"./app.1cccdb79.js";const s={},a={class:"mb-8"};function _(m,i){const e=r("router-view");return t(),c("main",a,[n(e)])}var l=o(s,[["render",_]]);export{l as default};
