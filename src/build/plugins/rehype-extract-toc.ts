8455cc4ac0070e8c7c92f7e3daaf71efc00fa86a
Yixuan-Wang <tom-yixuan_wang@qq.com>
2023-02-21 09:48:55 +0800

    style: lint

--- src/build/plugins/rehype-extract-toc.ts
+++ src/build/plugins/rehype-extract-toc.ts
@@ -5,12 +5,13 @@ import type { Plugin } from "unified";
 const rehypeExtractToc: Plugin<[], Root, [string, string][]> = function (this: Plugin<[], Root, [string, string][]>) {
   function compiler(tree: Root) {
     const headers: [string, string][] = [];
-    tree.children.forEach(child => {
-      if (child.type !== "element" || child.tagName !== "h2") return;
-      
+    tree.children.forEach((child) => {
+      if (child.type !== "element" || child.tagName !== "h2")
+        return;
+
       const h2 = toText(child).trim();
       headers.push([h2, child.properties?.id as string ?? ""]);
-    })
+    });
 
     return headers;
   }
