5ddfada0d7585a965cceb9d05b6f68b332925278
Yixuan-Wang <tom-yx_wang@outlook.com>
2023-12-17 21:31:18 -0600

    fix: placeholder for lab/typesetting

--- src/components/TypesettingFontFamily.svelte
+++ src/components/TypesettingFontFamily.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="grid w-full grid-cols-1 gap-2 items-center">
-  <input class="w-full" bind:value={$fontFamily.sans} placeholder="Inter, 'Noto Sans CJK SC'" />
-  <input class="w-full" bind:value={$fontFamily.serif} placeholder="'Source Serif 4', 'Noto Serif CJK SC'"/>
-  <input class="w-full" bind:value={$fontFamily.mono} placeholder="'JetBrains Mono', 'Noto Sans CJK SC'" />
+  <input class="w-full" bind:value={$fontFamily.sans} placeholder="Inter, 'Noto Sans SC'" />
+  <input class="w-full" bind:value={$fontFamily.serif} placeholder="'Source Serif 4', 'Noto Serif SC'"/>
+  <input class="w-full" bind:value={$fontFamily.mono} placeholder="'JetBrains Mono', 'Noto Sans SC'" />
 </div>
