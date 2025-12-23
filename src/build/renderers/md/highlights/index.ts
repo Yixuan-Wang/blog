da317987336873be1cceb5b89a9b24e9524d2da8
Yixuan-Wang <tom-yx_wang@outlook.com>
2025-12-23 02:37:39 -0500

    refactor: use glob import to load shiki grammar

--- src/build/renderers/md/highlights/index.ts
+++ src/build/renderers/md/highlights/index.ts
@@ -1,19 +1,19 @@
-import { readFileSync } from "node:fs";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
 import { getHighlighter, bundledLanguages } from "shiki/bundle/full";
 import { createCssVariablesTheme } from "shiki";
 
-const fetchGrammar = (lang: string) => JSON.parse(readFileSync(`src/build/renderers/md/highlights/languages/${lang}.tmLanguage.json`, { encoding: "utf-8" }))
+// @ts-ignore
+const langs = Object.values(import.meta.glob("./languages/*.tmLanguage.json", { query: "?raw", eager: true, import: "default" })).map(JSON.parse);
 
-// const theme = JSON.parse(readFileSync(resolve(__dirname, "theme.json"), { encoding: "utf-8" }))
+// const fetchGrammar = (lang: string) => JSON.parse(readFileSync(`src/build/renderers/md/highlights/languages/${lang}.tmLanguage.json`, { encoding: "utf-8" }))
 
-const langs = [
-  fetchGrammar("coq"),
-  // SOURCE: https://github.com/coq-community/vscoq/blob/main/client/syntax/coq.tmLanguage.json
-  fetchGrammar("koka"),
-  // SOURCE: https://github.com/koka-lang/koka/blob/dev/support/vscode/koka.language-koka/syntaxes/koka.json
-];
+// // const theme = JSON.parse(readFileSync(resolve(__dirname, "theme.json"), { encoding: "utf-8" }))
+
+// const langs = [
+//   fetchGrammar("coq"),
+//   // SOURCE: https://github.com/coq-community/vscoq/blob/main/client/syntax/coq.tmLanguage.json
+//   fetchGrammar("koka"),
+//   // SOURCE: https://github.com/koka-lang/koka/blob/dev/support/vscode/koka.language-koka/syntaxes/koka.json
+// ];
 
 // Create a custom CSS variables theme, the following are the default values
 const theme = createCssVariablesTheme({ 
