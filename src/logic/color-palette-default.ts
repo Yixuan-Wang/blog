00acd8b6706b62d08f2995ad1f3b838e13a7e715
Yixuan-Wang <tom-yx_wang@outlook.com>
2026-02-18 19:30:46 -0500

    feat: update default colors

--- src/logic/color-palette-default.ts
+++ src/logic/color-palette-default.ts
@@ -2,13 +2,13 @@ const DEFAULT_COLOR_PALETTE = {
   one: {
     mode: "oklch",
     l: 0.465,
-    c: 0.138,
-    h: 253.92,
+    c: 0.295,
+    h: 258,
   },
   two: {
     mode: "oklch",
     l: 0.6967,
-    c: 0.169,
+    c: 0.245,
     h: 237,
   },
 };
